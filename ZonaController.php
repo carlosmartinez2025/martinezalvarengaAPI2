7e59be4aa9a74c025166b681d4ede90c2eaab6d5
carlosmartinez2025 <161965321+carlosmartinez2025@users.noreply.github.com>
2025-10-23 11:32:44 -0600

    Add files via upload

---
+++ ZonaController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\zona;
+
+class ZonaController extends Controller
+{
+    //
+
+    public function obtenerZonas()
+    {
+        $Zona = new Zona();
+
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores =[];
+
+        $valores = $Zona::all();
+        
+        //VERIFICACION DE EXISTENCIA DE DATOS   
+        if(!empty($valores))
+        {
+            //SI SE ENCONTRARON DATOS
+            $satisfactorio = true;
+            $estado = 200;
+            $mensaje = "Valores encontrados";
+            $errores = 
+            [
+                "code" => 200,
+                "msg" => ""
+            ];
+        }
+        else
+        {
+            // NO SE ENCONTRARON DATOS
+            $satisfactorio = false;
+            $estado = 404;
+            $mensaje = "No se han encontrado valores";
+            $errores = 
+            [
+                "code" => 404,
+                "msg" => "Datos no encontrados"
+            ];
+        }
+
+        //SE CREA LA VARIABLE DE SALIDA
+        $respuesta = 
+        [
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
+            "data" => $valores,
+            "errors" => $errores,
+            "total" => sizeof($valores)
+        ];
+
+        //SE RETORNA EL MENSAJE AL USUARIO
+        return response()->json($respuesta,$estado);
+    }
+
+
+
+    public function obtenerZona(int $idzona = 0)
+    {
+
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores =[];
+
+        if($idzona > 0)
+        {
+            //EL PARAMETRO DE $idzona > 0
+            $Zona = new Zona();
+            $valores = $Zona->where('id_zona', $idzona)->get();
+
+            //VERIFICACION DE EXISTENCIA DE DATOS   
+             if(!empty($valores))
+            {
+             //SI SE ENCONTRARON DATOS
+            $satisfactorio = true;
+            $estado = 200;
+            $mensaje = "Valores encontrados";
+            $errores = 
+            [
+              "code" => 200,
+               "msg" => ""
+            ];
+         }
+         else
+         {
+            // NO SE ENCONTRARON DATOS
+            $satisfactorio = false;
+            $estado = 404;
+            $mensaje = "No se han encontrado valores";
+            $errores = 
+            [
+                "code" => 404,
+                "msg" => "Datos no encontrados"
+            ];
+        } //FIN DEL IF (!EMPTY(VALORES)){
+
+        }
+        else
+        {
+            // NO SE HA ENVIADO UN VALOR PARA EL PARAMETRO $idzona
+            $satisfactorio = false;
+            $estado = 400;
+            $mensaje = "No se ha enviado el parametro obligatorio";
+            $errores = 
+            [
+                "code" => 400,
+                "msg" => "El identificador de la zona esta vacio"
+            ];
+        }//FIN DEL IF($idzona . 0){
+        
+        //SE CREA LA VARIABLE DE SALIDA
+        $respuesta= [
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
+            "data" => $valores,
+            "errors" => $errores,
+            "total" => sizeof($valores)
+        ];
+
+        //SE MUESTRA EL MENSAJE AL USUARIO
+        return response()->json($respuesta,$estado);
+    }
+}
